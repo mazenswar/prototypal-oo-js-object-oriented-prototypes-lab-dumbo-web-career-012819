185c04fb2c76ce842c5f5e93e4d68ba7ed9c0ac9
mazenswar <mazenswar@protonmail.com>
2019-03-20 15:38:31 -0400

    oo prototypes

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+
+  veto(){
+    return 'No, I must disagree';
+  }
+  approve(){
+    return 'You can do that!';
+  }
+  doCharity(){
+    return "I like to help people.";
+  }
+
+  releasePressStatement(){
+    return "You will see great things from Scuber.";
+  }
+
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  }
+
+}
+
+
 // function BoardMember(name, homeState, training){
 //   this.name = name
 //   this.homeState = homeState
 //   this.training = training
 //
 //   this.veto = function(){
-//     return 'No, I must disagree'
+//     return 'No, I must disagree';
 //   }
 //   this.approve = function(){
-//     return 'You can do that!'
+//     return 'You can do that!';
 //   }
 //   this.doCharity = function(){
-//     return "I like to help people."
+//     return "I like to help people.";
 //   }
 //
 //   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
+//     return "You will see great things from Scuber.";
 //   }
 //
 //   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 //   }
 // }
